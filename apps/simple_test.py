e4c5775afaea7462c13d0d0c48dccd2acac564b5
Asif Ahmed <public.quickgrid@gmail.com>
2022-01-08 07:43:30 +0600

    windows openpose fix

--- apps/simple_test.py
+++ apps/simple_test.py
@@ -13,7 +13,7 @@ parser.add_argument('-i', '--input_path', type=str, default='./sample_images')
 parser.add_argument('-o', '--out_path', type=str, default='./results')
 parser.add_argument('-c', '--ckpt_path', type=str, default='./checkpoints/pifuhd.pt')
 parser.add_argument('-r', '--resolution', type=int, default=300)
-parser.add_argument('--use_rect', action='store_true', help='use rectangle for cropping')
+parser.add_argument('--use_rect', action='store_false', help='use rectangle for cropping')
 args = parser.parse_args()
 ###############################################################################################
 ##                   Upper PIFu
